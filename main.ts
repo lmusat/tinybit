c77e7c6e27712ceba2ad5c9a05c68d5b1530ead4
lmusat <lucian.musat@gmail.com>
2024-03-02 21:04:12 +0200

    more accurate check and react to value of ultrasonic distance

--- main.ts
+++ main.ts
@@ -1,59 +1,6 @@
 let distance = 0
 input.onButtonPressed(Button.A, function () {
-    distance = Tinybit.Ultrasonic_Car()
     basic.showNumber(distance)
-    if (distance < 1) {
-        basic.showLeds(`
-            . . . . .
-            . . . . .
-            . . . . .
-            . . . . .
-            # # # # #
-            `)
-        music.play(music.tonePlayable(988, music.beat(BeatFraction.Breve)), music.PlaybackMode.UntilDone)
-    } else {
-        if (distance < 10) {
-            basic.showLeds(`
-                . . . . .
-                . . . . .
-                . . . . .
-                # # # # #
-                # # # # #
-                `)
-            music.play(music.tonePlayable(784, music.beat(BeatFraction.Breve)), music.PlaybackMode.UntilDone)
-        } else {
-            if (distance < 20) {
-                basic.showLeds(`
-                    . . . . .
-                    . . . . .
-                    # # # # #
-                    # # # # #
-                    # # # # #
-                    `)
-                music.play(music.tonePlayable(523, music.beat(BeatFraction.Breve)), music.PlaybackMode.UntilDone)
-            } else {
-                if (distance < 50) {
-                    basic.showLeds(`
-                        . . . . .
-                        # # # # #
-                        # # # # #
-                        # # # # #
-                        # # # # #
-                        `)
-                    music.play(music.tonePlayable(392, music.beat(BeatFraction.Breve)), music.PlaybackMode.UntilDone)
-                } else {
-                    basic.showLeds(`
-                        # # # # #
-                        # # # # #
-                        # # # # #
-                        # # # # #
-                        # # # # #
-                        `)
-                    music.play(music.tonePlayable(247, music.beat(BeatFraction.Breve)), music.PlaybackMode.UntilDone)
-                }
-            }
-        }
-    }
 })
 input.onButtonPressed(Button.B, function () {
     basic.showLeds(`
@@ -113,5 +60,211 @@ input.onButtonPressed(Button.B, function () {
     basic.showIcon(IconNames.Heart)
 })
 basic.forever(function () {
-	
+    distance = Tinybit.Ultrasonic_Car()
+    if (distance <= 2) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            # . . . .
+            `)
+        music.play(music.tonePlayable(988, music.beat(BeatFraction.Half)), music.PlaybackMode.UntilDone)
+    } else {
+        if (distance <= 4) {
+            basic.showLeds(`
+                . . . . .
+                . . . . .
+                . . . . .
+                . . . . .
+                # # . . .
+                `)
+            music.play(music.tonePlayable(880, music.beat(BeatFraction.Half)), music.PlaybackMode.UntilDone)
+        } else {
+            if (distance <= 6) {
+                basic.showLeds(`
+                    . . . . .
+                    . . . . .
+                    . . . . .
+                    . . . . .
+                    # # # . .
+                    `)
+                music.play(music.tonePlayable(784, music.beat(BeatFraction.Half)), music.PlaybackMode.UntilDone)
+            } else {
+                if (distance <= 8) {
+                    basic.showLeds(`
+                        . . . . .
+                        . . . . .
+                        . . . . .
+                        . . . . .
+                        # # # # .
+                        `)
+                    music.play(music.tonePlayable(698, music.beat(BeatFraction.Half)), music.PlaybackMode.UntilDone)
+                } else {
+                    if (distance <= 10) {
+                        basic.showLeds(`
+                            . . . . .
+                            . . . . .
+                            . . . . .
+                            . . . . .
+                            # # # # #
+                            `)
+                        music.play(music.tonePlayable(659, music.beat(BeatFraction.Half)), music.PlaybackMode.UntilDone)
+                    } else {
+                        if (distance <= 15) {
+                            basic.showLeds(`
+                                . . . . .
+                                . . . . .
+                                . . . . .
+                                # . . . .
+                                # # # # #
+                                `)
+                            music.play(music.tonePlayable(587, music.beat(BeatFraction.Half)), music.PlaybackMode.UntilDone)
+                        } else {
+                            if (distance <= 20) {
+                                basic.showLeds(`
+                                    . . . . .
+                                    . . . . .
+                                    . . . . .
+                                    # # . . .
+                                    # # # # #
+                                    `)
+                                music.play(music.tonePlayable(523, music.beat(BeatFraction.Half)), music.PlaybackMode.UntilDone)
+                            } else {
+                                if (distance <= 25) {
+                                    basic.showLeds(`
+                                        . . . . .
+                                        . . . . .
+                                        . . . . .
+                                        # # # # .
+                                        # # # # #
+                                        `)
+                                    music.play(music.tonePlayable(494, music.beat(BeatFraction.Half)), music.PlaybackMode.UntilDone)
+                                } else {
+                                    if (distance <= 30) {
+                                        basic.showLeds(`
+                                            . . . . .
+                                            . . . . .
+                                            . . . . .
+                                            # # # # #
+                                            # # # # #
+                                            `)
+                                        music.play(music.tonePlayable(440, music.beat(BeatFraction.Half)), music.PlaybackMode.UntilDone)
+                                    } else {
+                                        if (distance <= 35) {
+                                            basic.showLeds(`
+                                                . . . . .
+                                                . . . . .
+                                                # . . . .
+                                                # # # # #
+                                                # # # # #
+                                                `)
+                                            music.play(music.tonePlayable(392, music.beat(BeatFraction.Half)), music.PlaybackMode.UntilDone)
+                                        } else {
+                                            if (distance <= 40) {
+                                                basic.showLeds(`
+                                                    . . . . .
+                                                    . . . . .
+                                                    # # . . .
+                                                    # # # # #
+                                                    # # # # #
+                                                    `)
+                                                music.play(music.tonePlayable(349, music.beat(BeatFraction.Half)), music.PlaybackMode.UntilDone)
+                                            } else {
+                                                if (distance <= 45) {
+                                                    basic.showLeds(`
+                                                        . . . . .
+                                                        . . . . .
+                                                        # # # . .
+                                                        # # # # #
+                                                        # # # # #
+                                                        `)
+                                                    music.play(music.tonePlayable(330, music.beat(BeatFraction.Half)), music.PlaybackMode.UntilDone)
+                                                } else {
+                                                    if (distance <= 50) {
+                                                        basic.showLeds(`
+                                                            . . . . .
+                                                            . . . . .
+                                                            # # # # .
+                                                            # # # # #
+                                                            # # # # #
+                                                            `)
+                                                        music.play(music.tonePlayable(294, music.beat(BeatFraction.Half)), music.PlaybackMode.UntilDone)
+                                                    } else {
+                                                        if (distance <= 55) {
+                                                            basic.showLeds(`
+                                                                . . . . .
+                                                                . . . . .
+                                                                # # # # #
+                                                                # # # # #
+                                                                # # # # #
+                                                                `)
+                                                            music.play(music.tonePlayable(262, music.beat(BeatFraction.Half)), music.PlaybackMode.UntilDone)
+                                                        } else {
+                                                            if (distance <= 60) {
+                                                                basic.showLeds(`
+                                                                    . . . . .
+                                                                    # . . . .
+                                                                    # # # # #
+                                                                    # # # # #
+                                                                    # # # # #
+                                                                    `)
+                                                                music.play(music.tonePlayable(247, music.beat(BeatFraction.Half)), music.PlaybackMode.UntilDone)
+                                                            } else {
+                                                                if (distance <= 70) {
+                                                                    basic.showLeds(`
+                                                                        . . . . .
+                                                                        # # # . .
+                                                                        # # # # #
+                                                                        # # # # #
+                                                                        # # # # #
+                                                                        `)
+                                                                    music.play(music.tonePlayable(220, music.beat(BeatFraction.Half)), music.PlaybackMode.UntilDone)
+                                                                } else {
+                                                                    if (distance <= 80) {
+                                                                        basic.showLeds(`
+                                                                            . . . . .
+                                                                            # # # # #
+                                                                            # # # # #
+                                                                            # # # # #
+                                                                            # # # # #
+                                                                            `)
+                                                                        music.play(music.tonePlayable(196, music.beat(BeatFraction.Half)), music.PlaybackMode.UntilDone)
+                                                                    } else {
+                                                                        if (distance <= 100) {
+                                                                            basic.showLeds(`
+                                                                                # # # . .
+                                                                                # # # # #
+                                                                                # # # # #
+                                                                                # # # # #
+                                                                                # # # # #
+                                                                                `)
+                                                                            music.play(music.tonePlayable(175, music.beat(BeatFraction.Half)), music.PlaybackMode.UntilDone)
+                                                                        } else {
+                                                                            basic.showLeds(`
+                                                                                # # # # #
+                                                                                # # # # #
+                                                                                # # # # #
+                                                                                # # # # #
+                                                                                # # # # #
+                                                                                `)
+                                                                            music.play(music.tonePlayable(131, music.beat(BeatFraction.Half)), music.PlaybackMode.UntilDone)
+                                                                        }
+                                                                    }
+                                                                }
+                                                            }
+                                                        }
+                                                    }
+                                                }
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
 })
