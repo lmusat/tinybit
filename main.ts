8e9c6e0bf70f0417072699fbdf01f35444da55a4
lmusat <lucian.musat@gmail.com>
2024-03-02 16:05:00 +0200

    detectează distanța până la obstacol în față

--- main.ts
+++ main.ts
@@ -1,5 +1,59 @@
+let distance = 0
 input.onButtonPressed(Button.A, function () {
-	
+    distance = Tinybit.Ultrasonic_Car()
+    basic.showNumber(distance)
+    if (distance < 1) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            # # # # #
+            `)
+        music.play(music.tonePlayable(988, music.beat(BeatFraction.Breve)), music.PlaybackMode.UntilDone)
+    } else {
+        if (distance < 10) {
+            basic.showLeds(`
+                . . . . .
+                . . . . .
+                . . . . .
+                # # # # #
+                # # # # #
+                `)
+            music.play(music.tonePlayable(784, music.beat(BeatFraction.Breve)), music.PlaybackMode.UntilDone)
+        } else {
+            if (distance < 20) {
+                basic.showLeds(`
+                    . . . . .
+                    . . . . .
+                    # # # # #
+                    # # # # #
+                    # # # # #
+                    `)
+                music.play(music.tonePlayable(523, music.beat(BeatFraction.Breve)), music.PlaybackMode.UntilDone)
+            } else {
+                if (distance < 50) {
+                    basic.showLeds(`
+                        . . . . .
+                        # # # # #
+                        # # # # #
+                        # # # # #
+                        # # # # #
+                        `)
+                    music.play(music.tonePlayable(392, music.beat(BeatFraction.Breve)), music.PlaybackMode.UntilDone)
+                } else {
+                    basic.showLeds(`
+                        # # # # #
+                        # # # # #
+                        # # # # #
+                        # # # # #
+                        # # # # #
+                        `)
+                    music.play(music.tonePlayable(247, music.beat(BeatFraction.Breve)), music.PlaybackMode.UntilDone)
+                }
+            }
+        }
+    }
 })
 input.onButtonPressed(Button.B, function () {
     basic.showLeds(`
